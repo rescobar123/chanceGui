2c2ce809537a749621e3fdc87d21f363eba48d3f
Robin Escobar <robinsolises@hotmail.com>
2022-02-14 11:16:11 -0600

    barra de estado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,8 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.statusBar.styleLightContent();
+      this.statusBar.backgroundColorByHexString("#3c5ed5");
       this.splashScreen.hide();
     });
   }
